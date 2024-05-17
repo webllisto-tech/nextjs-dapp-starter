d8b9de909b804e234c95f16a9ebc478fc8bb26b7
Deepak Verma <deepak@webllisto.com>
2024-05-17 17:39:24 +0530

    add wallet connectivity

--- src/design-systems/Templates/HomePageTemplate/index.tsx
+++ src/design-systems/Templates/HomePageTemplate/index.tsx
@@ -1,13 +1,10 @@
 'use client'
-import { useWeb3Modal, useWeb3ModalState } from '@web3modal/wagmi/react'
 import { useAccount } from 'wagmi'
 
-import Button from 'design-systems/Atoms/Button'
 import Typography from 'design-systems/Atoms/Typography'
 import Layout from 'design-systems/Organisms/Layout'
 
 const HomePageTemplate: React.FC = () => {
-  const { open } = useWeb3Modal()
   const { address, isConnected } = useAccount()
 
   return (
